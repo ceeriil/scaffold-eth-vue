c0943efcf904d3ac3c70fcafc62d149b4b21902f
ceeriil <simonorugbo15@gmail.com>
2024-04-22 18:42:59 -0700

    Configure Tailwind

--- packages/nuxt/components/Tutorial.vue
+++ packages/nuxt/components/Tutorial.vue
@@ -1,12 +1,60 @@
-<!-- Please remove this file from your project -->
 <template>
-  <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
-  >
-    <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
+  <div class="flex items-center flex-col flex-grow pt-10">
+    <div class="px-5">
+      <h1 class="text-center mb-8">
+        <span class="block text-2xl mb-2">Welcome to</span>
+        <span class="block text-4xl font-bold">Scaffold-ETH 2 Vue</span>
+      </h1>
+      <p class="text-center text-lg">
+        Get started by editing{" "}
+        <code
+          class="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block"
+        >
+          packages/nuxtjs/app/page.tsx
+        </code>
+      </p>
+      <p class="text-center text-lg">
+        Edit your smart contract{" "}
+        <code
+          class="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block"
+        >
+          YourContract.sol </code
+        >{" "} in{" "}
+        <code
+          class="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block"
+        >
+          packages/hardhat/contracts
+        </code>
+      </p>
+    </div>
+
+    <div class="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+      <div class="flex justify-center items-center gap-12 flex-col sm:flex-row">
+        <div
+          class="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl"
+        >
+          <BugAntIcon className="h-8 w-8 fill-secondary" />
+
+          <p>
+            Tinker with your smart contract using the{" "}
+            <NuxtLink to="/debug" passHref class="link">
+              Debug Contract </NuxtLink
+            >{" "} tab.
+          </p>
+        </div>
+        <div
+          class="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl"
+        >
+          <MagnifyingGlassIcon class="h-8 w-8 fill-secondary" />
+          <p>
+            Explore your local transactions with the{" "}
+            <NuxtLink to="/blockexplorer" passHref class="link">
+              Block Explorer </NuxtLink
+            >{" "} tab.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
